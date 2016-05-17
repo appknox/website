83cb9d66e601d4de8a028799b9376e31123514ee
Subho Halder <subho.halder@gmail.com>
2016-05-17 13:54:54 -0700

    Reverted and deleted header fix for now

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ MenuManager.prototype.positionDropMenu = function(){
 		offsetTop = document.body.scrollTop || document.documentElement.scrollTop;
 	}
 
-	offsetTop = -offsetTop + headerHeight + headerTop;
+	offsetTop = -offsetTop + headerHeight;
 	this.dropMenus.css("top",offsetTop);
 }
 
@@ -2060,8 +2060,6 @@ function sendLandingPageForm1(ev){
 function bindLandingPageForm1(){
 	$("#landing_page_1_submit").unbind("click",sendLandingPageForm1).bind("click",sendLandingPageForm1);
 }
-
-
 
 
 
