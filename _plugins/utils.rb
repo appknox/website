ef1ef8faa95a3d5abfca6664b977cae1a754f4b7
yashwin <yashwinpoojary@gmail.com>
2016-06-14 19:39:43 +0530

    Custom Tags In Jekyll Using Ruby
    Please don't merge this as there is some issue with this. It would be
    great if you can help me with this.

---
+++ _plugins/utils.rb
@@ -0,0 +1,46 @@
+module Jekyll
+
+        class AKIconTag < Liquid::Tag
+
+              def initialize(tag_name,text,tokens)
+                  super
+                  @text = text
+              end
+
+              def render(context)
+                  case @text
+                  when /linkedin/
+                        "linkedin"
+                  when /twitter/
+                        "twitter"
+                  when /facebook/
+                        "facebook"
+                  when /github/
+                        "github"
+                  when /@/
+                        "envelope"
+                  else
+                        "globe"
+                  end
+              end
+        end
+
+        class AKLinkTag < Liquid::Tag
+
+              def initialize(tag_name,text,tokens)
+                  super
+                  @text = text
+              end
+
+              def render(context)
+                  if @text.include? "@"
+                        "mailto://#{@text}"
+                  else
+                      "#{@text}"
+                  end
+              end
+        end
+end
+
+Liquid::Template.register_tag('ak_icon',Jekyll::AKIconTag)
+Liquid::Template.register_tag('ak_link',Jekyll::AKLinkTag)
