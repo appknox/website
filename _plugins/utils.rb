9fbcb047f36eb54e413942008e54912fc2b23f47
yashwin <yashwinpoojary@gmail.com>
2016-06-16 18:54:06 +0530

    custom-jekyll-tags
    Updated!!

--- _plugins/utils.rb
+++ _plugins/utils.rb
@@ -1,46 +1,43 @@
 module Jekyll
+  class AKIconTag < Liquid::Tag
 
-        class AKIconTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
 
-              def initialize(tag_name,text,tokens)
-                  super
-                  @text = text
-              end
+    def render(context)
+      case @text
+      when /linkedin/
+        'linkedin'
+      when /facebook/
+        'facebook'
+      when /twitter/
+        'twitter'
+      when /@/
+        'envelope'
+      else
+        'globe'
+      end
+    end
+  end
+  
+  class AKLinkTag < Liquid::Tag
 
-              def render(context)
-                  case @text
-                  when /linkedin/
-                        "linkedin"
-                  when /twitter/
-                        "twitter"
-                  when /facebook/
-                        "facebook"
-                  when /github/
-                        "github"
-                  when /@/
-                        "envelope"
-                  else
-                        "globe"
-                  end
-              end
-        end
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
 
-        class AKLinkTag < Liquid::Tag
-
-              def initialize(tag_name,text,tokens)
-                  super
-                  @text = text
-              end
-
-              def render(context)
-                  if @text.include? "@"
-                        "mailto://#{@text}"
-                  else
-                      "#{@text}"
-                  end
-              end
-        end
+    def render(context)
+      if @text.include? "@"
+        "mailto://#{@text}"
+      else
+          "#{@text}"
+      end
+    end
+  end
 end
 
-Liquid::Template.register_tag('ak_icon',Jekyll::AKIconTag)
-Liquid::Template.register_tag('ak_link',Jekyll::AKLinkTag)
+Liquid::Template.register_tag('context_icon', Jekyll::AKIconTag)
+Liquid::Template.register_tag('context_link', Jekyll::AKLinkTag)
