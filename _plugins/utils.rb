d357a5439bc99d48fc6082c9e571cbdafb18062e
Subho Halder <subho.halder@gmail.com>
2016-07-04 12:33:01 +0530

    small fixes

--- _plugins/utils.rb
+++ _plugins/utils.rb
@@ -1,23 +1,22 @@
+=begin
+Utils
+=end
 module Jekyll
   module AKUtils
     def ak_icon(text)
       case text
-      when /linkedin/
-        'linkedin'
-      when /facebook/
-        'facebook'
-      when /twitter/
-        'twitter'
-      when /@/
-        'envelope'
-      else
-        'globe'
+      when /linkedin/ then 'linkedin'
+      when /facebook/ then 'facebook'
+      when /twitter/ then 'twitter'
+      when /github.com/ then 'github'
+      when /@/ then 'envelope'
+      else 'globe'
       end
     end
 
     def ak_link(text)
       if text.include? '@'
-        "mailto://#{text}"
+        "mailto:#{text}"
       else
         text.to_s
       end
