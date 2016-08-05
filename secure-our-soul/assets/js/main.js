12a16e3b91f39770ee51980f618dec6d6f722d52
yashwin <yashwinpoojary@gmail.com>
2016-08-05 17:25:12 +0530

    redirecting the buttons

---
+++ secure-our-soul/assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+$(".book_section").click(function() {
+       window.location.href="#submit_section";
+       e.preventDefault();
+       e.stopPropagation();
+});
+});
