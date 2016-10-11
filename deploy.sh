453893815bfc76895ea473ca1ccf3078652b1c2e
Subho Halder <subho.halder@gmail.com>
2016-10-12 03:31:44 +0800

    fix: improve the deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,8 @@
 # Distributed under terms of the MIT license.
 #
 
+bundle exec jekyll build
+git add _site/*
+git commit -m "auto: deploying to github pages"
+git push origin $(git rev-parse --abbrev-ref HEAD)
 git subtree push --prefix _site/ origin gh-pages
